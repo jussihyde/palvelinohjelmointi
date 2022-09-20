1d72bd0cb064bbd7e01bd1576a4e4eab4fa0f5e2
Jussi <Jussi@MacBook-Pro-2.local>
2022-09-21 02:16:58 +0300

    add, delete functions added

--- bookstore/src/main/java/palvelinohjelmointi/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/palvelinohjelmointi/bookstore/web/BookstoreController.java
@@ -3,8 +3,11 @@ package palvelinohjelmointi.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import palvelinohjelmointi.bookstore.domain.Book;
 import palvelinohjelmointi.bookstore.domain.BookRepository;
 
 @Controller
@@ -23,6 +26,24 @@ public class BookstoreController {
 		model.addAttribute("books", repository.findAll());
 		
 		return "/booklist";
+	}
+	@RequestMapping(value = "/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		
+		return "redirect:../booklist";
+	}
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		
+		return "addstudent";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		repository.save(book);
+		
+		return "redirect:booklist";
 	}
 	
 
